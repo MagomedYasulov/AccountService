248a78f8b9b0d64714f7c73aad7c4e6d605fd95e
magomed <magomedyasulov8@gmail.com>
2025-08-09 00:50:23 +0300

    task3: добавил unit тесты

---
+++ AccountService.Tests/UnitTests/AccountAutoMapperProfileTests.cs
@@ -0,0 +1,142 @@
+﻿using AccountService.Domain.Data.Entities;
+using AccountService.Domain.Enums;
+using AccountService.Features.Accounts.Models;
+using AutoMapper;
+
+namespace AccountService.Tests.UnitTests
+{
+    public class AccountAutoMapperProfileTests
+    {
+        private IMapper _mapper;
+
+        public AccountAutoMapperProfileTests()
+        {
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new AccountAutoMapperProfile()));
+            _mapper = new Mapper(mapperConfig);
+        }
+
+
+        [Fact]
+        public void Map_Account_To_AccountDto()
+        {
+            // Arrange
+            var account = new Account()
+            {
+                Id = Guid.NewGuid(),
+                Balance = 424,
+                ClosedAt = DateTime.UtcNow,
+                OpenedAt = new DateTime(2025, 02, 2),
+                OwnerId = Guid.NewGuid(),
+                CurrencyCode = "RUB",
+                InterestRate = 23,
+                Type = AccountType.Credit,
+                Revoked = true,
+            };
+
+            // Act
+            var dto = _mapper.Map<AccountDto>(account);
+
+            // Assert
+            Assert.Equal(account.Id, dto.Id);
+            Assert.Equal(account.Balance, dto.Balance);
+            Assert.Equal(account.ClosedAt, dto.ClosedAt);
+            Assert.Equal(account.OpenedAt, dto.OpenedAt);
+            Assert.Equal(account.OwnerId, dto.OwnerId);
+            Assert.Equal(account.CurrencyCode, dto.CurrencyCode);
+            Assert.Equal(account.InterestRate, dto.InterestRate);
+            Assert.Equal(account.Type, dto.Type);
+            Assert.Equal(account.Revoked, dto.Revoked);
+        }
+
+        [Fact]
+        public void Map_Transaction_To_TransactionStatementDto()
+        {
+            // Arrange
+            var account = new Transaction()
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.NewGuid(),
+                CounterpartyAccountId = Guid.NewGuid(),
+                CurrencyCode = "EUR",
+                Description = "Transaction desc",
+                Sum = 564,
+                Type = TransactionType.Credit,
+                TransferTime = DateTime.UtcNow,
+            };
+
+            // Act
+            var dto = _mapper.Map<TransactionStatementDto>(account);
+
+            // Assert
+            Assert.Equal(account.CounterpartyAccountId, dto.CounterpartyAccountId);
+            Assert.Equal(account.Description, dto.Description);
+            Assert.Equal(account.Sum, dto.Sum);
+            Assert.Equal(account.CurrencyCode, dto.CurrencyCode);
+            Assert.Equal(account.TransferTime, dto.TransferTime);
+            Assert.Equal(account.Type, dto.Type);
+        }
+
+        [Fact]
+        public void Map_Account_To_AccountStatementDto()
+        {
+            // Arrange
+            var counterpartyAccountId = Guid.NewGuid();
+            var accountId = Guid.NewGuid();
+            var transferTime = DateTime.UtcNow;
+
+            var account = new Account()
+            {
+                Id = accountId,
+                Balance = 424,
+                ClosedAt = DateTime.UtcNow,
+                OpenedAt = new DateTime(2025, 02, 2),
+                OwnerId = Guid.NewGuid(),
+                CurrencyCode = "RUB",
+                InterestRate = 23,
+                Type = AccountType.Credit,
+                Revoked = true,
+                Transactions = [
+                    new Transaction()
+                    {
+                        AccountId = accountId,
+                        CounterpartyAccountId = counterpartyAccountId,
+                        CurrencyCode = "EUR",
+                        Description = "Transaction desc",
+                        Sum = 564,
+                        Type = TransactionType.Credit,
+                        TransferTime = transferTime,
+                    }
+                ],
+                CounterpartyTransactions = [
+                    new Transaction()
+                    {
+                        AccountId = counterpartyAccountId,
+                        CounterpartyAccountId = accountId,
+                        CurrencyCode = "EUR",
+                        Description = "Transaction desc",
+                        Sum = 564,
+                        Type = TransactionType.Debit, //Для аккаунта в транзакциях будет наоборот Credit
+                        TransferTime = transferTime,
+                    }
+                ]
+            };
+
+            // Act
+            var dto = _mapper.Map<AccountStatementDto>(account);
+
+            // Assert
+            Assert.Equal(account.Balance, dto.Balance);
+            Assert.Equal(account.CurrencyCode, dto.CurrencyCode);
+
+            foreach (var transaction in dto.Transactions)
+            {
+                Assert.Equal(counterpartyAccountId, transaction.CounterpartyAccountId);
+                Assert.Equal(564, transaction.Sum);
+                Assert.Equal("EUR", transaction.CurrencyCode);
+                Assert.Equal("Transaction desc", transaction.Description);
+                Assert.Equal(transferTime, transaction.TransferTime);
+                Assert.Equal(TransactionType.Credit, transaction.Type);
+            }
+        }
+    }
+}
---
+++ AccountService.Tests/UnitTests/TransactionAutoMapperProfileTests.cs
@@ -0,0 +1,103 @@
+﻿using AccountService.Domain.Data.Entities;
+using AccountService.Domain.Enums;
+using AccountService.Features.Accounts.Models;
+using AccountService.Features.Transactions.CreateTransaction;
+using AccountService.Features.Transactions.Models;
+using AutoMapper;
+
+namespace AccountService.Tests.UnitTests
+{   
+    public class TransactionAutoMapperProfileTests
+    {
+        private IMapper _mapper;
+
+        public TransactionAutoMapperProfileTests()
+        {
+            var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new TransactionAutoMapperProfile()));
+            _mapper = new Mapper(mapperConfig);
+        }
+
+        [Fact]
+        public void Map_Transaction_To_TransactionDto()
+        {
+            // Arrange
+            var transaction = new Transaction()
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.NewGuid(),
+                CounterpartyAccountId = Guid.NewGuid(),
+                TransferTime = DateTime.UtcNow,
+                CurrencyCode = "RUB",
+                Description = "transaction desc",
+                Sum = 796,               
+                Type = TransactionType.Credit,
+            };
+
+            // Act
+            var dto = _mapper.Map<TransactionDto>(transaction);
+
+            // Assert
+            Assert.Equal(transaction.Id, dto.Id);
+            Assert.Equal(transaction.AccountId, dto.AccountId);
+            Assert.Equal(transaction.CounterpartyAccountId, dto.CounterpartyAccountId);
+            Assert.Equal(transaction.TransferTime, dto.TransferTime);
+            Assert.Equal(transaction.CurrencyCode, dto.CurrencyCode);
+            Assert.Equal(transaction.Description, dto.Description);
+            Assert.Equal(transaction.Type, dto.Type);
+            Assert.Equal(transaction.Sum, dto.Sum);
+        }
+
+        [Fact]
+        public void Map_CreateTransactionDto_To_CreateTransactionCommand()
+        {
+            // Arrange
+            var createTransactionDto = new CreateTransactionDto()
+            {
+                AccountId = Guid.NewGuid(),
+                CounterpartyAccountId = Guid.NewGuid(),
+                CurrencyCode = "RUB",
+                Description = "transaction desc",
+                Sum = 796,
+                Type = TransactionType.Credit,
+            };
+
+            // Act
+            var dto = _mapper.Map<CreateTransactionCommand>(createTransactionDto);
+
+            // Assert
+            Assert.Equal(createTransactionDto.AccountId, dto.AccountId);
+            Assert.Equal(createTransactionDto.CounterpartyAccountId, dto.CounterpartyAccountId);
+            Assert.Equal(createTransactionDto.CurrencyCode, dto.CurrencyCode);
+            Assert.Equal(createTransactionDto.Description, dto.Description);
+            Assert.Equal(createTransactionDto.Type, dto.Type);
+            Assert.Equal(createTransactionDto.Sum, dto.Sum);
+        }
+
+        [Fact]
+        public void Map_CreateTransactionCommand_To_Transaction()
+        {
+            // Arrange
+            var createTransactionCommand = new CreateTransactionCommand()
+            {
+                AccountId = Guid.NewGuid(),
+                CounterpartyAccountId = Guid.NewGuid(),
+                CurrencyCode = "RUB",
+                Description = "transaction desc",
+                Sum = 796,
+                Type = TransactionType.Credit,
+            };
+
+            // Act
+            var dto = _mapper.Map<Transaction>(createTransactionCommand);
+
+            // Assert
+            Assert.Equal(createTransactionCommand.AccountId, dto.AccountId);
+            Assert.Equal(createTransactionCommand.CounterpartyAccountId, dto.CounterpartyAccountId);
+            Assert.Equal(createTransactionCommand.CurrencyCode, dto.CurrencyCode);
+            Assert.Equal(createTransactionCommand.Description, dto.Description);
+            Assert.Equal(createTransactionCommand.Type, dto.Type);
+            Assert.Equal(createTransactionCommand.Sum, dto.Sum);
+        }
+
+    }
+}
