d18c22f1c236af20ccb91377e01464e29f79c1c6
magomed <magomedyasulov8@gmail.com>
2025-08-09 02:15:01 +0300

    task3: подготовил среду для написания интеграционного теста

---
+++ AccountService.Tests/Extentions/AppDbContextExtentions.cs
@@ -0,0 +1,64 @@
+﻿using AccountService.Domain.Data.Entities;
+using AccountService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AccountService.Tests.Extentions
+{
+    public static class AppDbContextExtentions
+    {
+        public static void SeedData(this AppDbContext dbContext)
+        {
+            var account1 = new Account()
+            {
+                Balance = 500000,
+                CurrencyCode = "RUB",
+                Type = Domain.Enums.AccountType.Checking,
+                OwnerId = Guid.NewGuid(),
+            };
+
+            var account2 = new Account()
+            {
+                Balance = 500000,
+                CurrencyCode = "RUB",
+                Type = Domain.Enums.AccountType.Checking,
+                OwnerId = Guid.NewGuid(),
+            };
+
+
+            dbContext.Accounts.AddRange(account1, account2);
+            dbContext.SaveChanges();
+        }
+
+        public static AppDbContext MigrateDB(this AppDbContext dbContext, ILogger<AppDbContext> logger)
+        {
+            int maxRetryAttempts = 5;
+            TimeSpan retryDelay = TimeSpan.FromSeconds(3);
+
+            for (int attempt = 1; attempt <= maxRetryAttempts; attempt++)
+            {
+                try
+                {
+                    logger.LogInformation("Applying migrations (Attempt {Attempt}/{MaxAttempts})...", attempt, maxRetryAttempts);
+                    dbContext.Database.Migrate();
+                    logger.LogInformation("Migrations applied successfully!");
+                    return dbContext;
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Migration attempt {Attempt}/{MaxAttempts} failed.", attempt, maxRetryAttempts);
+
+                    if (attempt == maxRetryAttempts)
+                    {
+                        logger.LogCritical("All migration attempts failed. Application will exit.");
+                        throw;
+                    }
+
+                    Thread.Sleep(retryDelay);
+                }
+            }
+
+            return dbContext;
+        }
+    }
+}
---
+++ AccountService.Tests/IntegrationTests/TransactionApiTests.cs
@@ -0,0 +1,124 @@
+﻿using AccountService.Infrastructure.Data;
+using AccountService.Tests.Extentions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Testcontainers.PostgreSql;
+
+namespace AccountService.Tests.IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("password")
+            .WithImage("postgres:latest")
+            .Build();
+
+        public async Task InitializeAsync()
+        {
+            await _postgres.StartAsync();
+        }
+
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _postgres.DisposeAsync();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Удаляем все зарегистрированные схемы аутентификации
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "Test";
+                    options.DefaultChallengeScheme = "Test";
+                })
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", options => { });
+
+                // Можно убрать авторизацию, добавив политику, которая всегда разрешает
+                services.AddAuthorizationBuilder().AddPolicy("AllowAll", policy =>
+                {
+                    policy.RequireAssertion(_ => true);
+                });
+            });
+
+
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    ["ConnectionStrings:DefaultConnection"] = _postgres.GetConnectionString()
+                });
+            });
+
+            builder.ConfigureTestServices(services =>
+            {
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<AppDbContext>>();
+                
+                db.MigrateDB(logger).SeedData();
+            });
+        }
+    }
+
+    [CollectionDefinition("Integration tests")]
+    public class IntegrationTestCollection : ICollectionFixture<CustomWebApplicationFactory> { }
+
+    [Collection("Integration tests")]
+    public class TransactionApiTests //: IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly CustomWebApplicationFactory _factory;
+
+        public TransactionApiTests(CustomWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+
+        [Fact]
+        public async Task Parraler_Transaction_MustPreservationTotalBalance()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/v1/accounts");
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+
+    // Тестовый обработчик, который всегда аутентифицирует пользователя
+    public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+                               ILoggerFactory logger,
+                               UrlEncoder encoder) : base(options, logger, encoder)
+
+        { }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[] { new Claim(ClaimTypes.Name, "Test user") };
+            var identity = new ClaimsIdentity(claims, "Test");
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, "Test");
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
