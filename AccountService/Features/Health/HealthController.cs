da430b75e68a462a5f3c4f2df2a539e564d30896
magomed <magomedyasulov8@gmail.com>
2025-08-17 15:32:05 +0300

    task4: изменил немного url health check

--- AccountService/Features/Health/HealthController.cs
+++ AccountService/Features/Health/HealthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AccountService.Features.Health
 {
-    [Route("api/v1/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     [TypeFilter<ApiExceptionFilter>]
     public class HealthController : ControllerBase
